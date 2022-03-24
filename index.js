1673931d581b7b33f0c65782b3c0f357cf3d4982
viet_v <vohoangviet459@gmail.com>
2022-03-24 14:25:24 +0700

    Added overlay on main site when view navigation

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const hamburger = document.querySelector('.nav--hamburger');
-const hamburgerBars = document.querySelectorAll('.line');
-const navSection = document.querySelector('.nav--section');
+const hamburger = document.querySelector('.nav--hamburger');    // hamburger menu
+const hamburgerBars = document.querySelectorAll('.line');       // hamburger menu bars
+const navSection = document.querySelector('.nav--section');     // navigation section
+const navOverlay = document.querySelector('.nav-overlay');      // slight overlay on screen when open navigation
 
 
-hamburger.addEventListener('click', function () {
+hamburger.addEventListener('click', function () {         // click on hamburger to open/close navigation
     hamburgerBars.forEach(bar => bar.classList.toggle('color-change'));
     hamburger.classList.toggle('rotate');
     navSection.classList.toggle('slideIn');
+    navOverlay.classList.toggle('display');
+})
+
+navOverlay.addEventListener('click', function () {        // click on overlay to close navigation
+    hamburgerBars.forEach(bar => bar.classList.toggle('color-change'));
+    hamburger.classList.toggle('rotate');
+    navSection.classList.toggle('slideIn');
+    navOverlay.classList.toggle('display');
 })
 
