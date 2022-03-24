a8f8284a15a1fda028c26d97f81b898004f67ab9
viet_v <vohoangviet459@gmail.com>
2022-03-24 13:30:44 +0700

    Added fade in effect for service content. Position fixed the navigation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.nav--hamburger');
 const hamburgerBars = document.querySelectorAll('.line');
 const navSection = document.querySelector('.nav--section');
-const body = document.querySelector('body');
+
 
 hamburger.addEventListener('click', function () {
     hamburgerBars.forEach(bar => bar.classList.toggle('color-change'));
